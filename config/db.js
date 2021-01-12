fc32a4ddb772040d6ff71d16749157b83cbb5e93
LocalHostIP <miguel.gro.padilla@gmail.com>
2021-01-11 18:37:58 -0600

    test db

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 //Data base url
 module.exports={
-    url:'mongodb://localhost/prototipo_empresac'
+    // url:'mongodb://localhost/prototipo_empresac'
+    url:'mongodb+srv://devAdmin:nmnPYRQds9syVr8k@pasionazul.pk5kw.mongodb.net/PasionAzul?retryWrites=true&w=majority'
 }
